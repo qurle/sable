326e588f852410d57d7b40919ad01c1dfd7ac8cf
nick <32414396+qurle@users.noreply.github.com>
2026-05-24 17:05:37 +0000

    Fixed errors in NeoForge implementation

--- neoforge/src/main/java/dev/ryanhcode/sable/neoforge/platform/SableLoaderPlatformImpl.java
+++ neoforge/src/main/java/dev/ryanhcode/sable/neoforge/platform/SableLoaderPlatformImpl.java
@@ -1,21 +1,6 @@
 package dev.ryanhcode.sable.neoforge.platform;
 
 import dev.ryanhcode.sable.platform.SableLoaderPlatform;
-import net.neoforged.fml.loading.LoadingModList;
-import org.jetbrains.annotations.Nullable;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-public class SableLoaderPlatformImpl implements SableLoaderPlatform {
-	@Override
-	public String getModVersion(final String modId) {
-		return LoadingModList.get().getModFileById(modId).versionString();
-	}
-
-package dev.ryanhcode.sable.neoforge.platform;
-
-import dev.ryanhcode.sable.platform.SableLoaderPlatform;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.fml.loading.LoadingModList;
 import org.jetbrains.annotations.Nullable;
@@ -34,4 +19,3 @@ public class SableLoaderPlatformImpl implements SableLoaderPlatform {
 		return FMLPaths.GAMEDIR.get();
 	}
 }
-
